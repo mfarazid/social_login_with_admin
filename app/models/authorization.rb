109e5f67ad712575b7e416dd89a3094b93cccfcb
Faraz Ali <mfarazid@gmail.com>
2014-09-20 11:44:09 -0500

    Authorization model cleanup

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,12 +3,9 @@ class Authorization < ActiveRecord::Base
 
   after_create :fetch_details
 
-
-
   def fetch_details
     self.send("fetch_details_from_#{self.provider.downcase}")
   end
-
 
   def fetch_details_from_facebook
     graph = Koala::Facebook::API.new(self.token)
@@ -17,20 +14,11 @@ class Authorization < ActiveRecord::Base
     self.save
   end
 
-  def fetch_details_from_twitter
+  # def fetch_details_from_twitter
 
-  end
+  # end
 
+  # def fetch_details_from_google_oauth2
 
-  def fetch_details_from_github
-  end
-
-
-  def fetch_details_from_linkedin
-
-  end
-
-  def fetch_details_from_google_oauth2
-
-  end
+  # end
 end
